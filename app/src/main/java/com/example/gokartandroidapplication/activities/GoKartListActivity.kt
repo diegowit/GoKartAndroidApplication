14c8faa254ac5e30a35f8a1c4ac3a56c44e91a20
diegowit <20098757@mail.wit.ie>
2024-03-16 06:34:38 +0000

    Main App file created

--- app/src/main/java/com/example/gokartandroidapplication/activities/GoKartListActivity.kt
+++ app/src/main/java/com/example/gokartandroidapplication/activities/GoKartListActivity.kt
@@ -1,22 +1,18 @@
 package com.example.gokartandroidapplication.activities
 
 
-import android.app.Activity
-import android.content.Intent
 import android.view.LayoutInflater
-import android.view.Menu
-import android.view.MenuItem
 import android.view.ViewGroup
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 import com.example.gokartandroidapplication.databinding.ActivityGokartlistBinding
-//import com.example.gokartandroidapplication.databinding.CardGoKartBinding
+import com.example.gokartandroidapplication.databinding.CardGokartBinding
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.gokartandroidapplication.R
 import com.example.gokartandroidapplication.main.MainApp
+import com.example.gokartandroidapplication.models.GoKartModel
 
 class GoKartListActivity : AppCompatActivity() {
 
@@ -29,8 +25,40 @@ class GoKartListActivity : AppCompatActivity() {
         setContentView(R.layout.activity_gokartlist)
         app = application as MainApp
 
-
-
-
+        val layoutManager = LinearLayoutManager(this)
+        binding.recyclerView.layoutManager = layoutManager
+        binding.recyclerView.adapter = GoKartAdapter(app.gokarts)
     }
 }
+
+class GoKartAdapter(private var gokarts: ArrayList<GoKartModel>) :
+    RecyclerView.Adapter<GoKartAdapter.MainHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainHolder {
+        val binding = CardGokartBinding
+            .inflate(LayoutInflater.from(parent.context), parent, false)
+
+        return MainHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: MainHolder, position: Int) {
+        val gokart = gokarts[holder.adapterPosition]
+        holder.bind(gokart)
+    }
+
+    override fun getItemCount(): Int = gokarts.size
+
+    class MainHolder(private val binding : CardGokartBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(gokart: GoKartModel) {
+            binding.DriverName.text = gokart.name
+            binding.CarModel.text = gokart.carModel
+        }
+    }
+}
+
+
+
+
+
