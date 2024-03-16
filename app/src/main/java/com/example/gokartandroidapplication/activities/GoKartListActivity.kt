e2e8f1c4e217d5262c54e1961073658f01c9f68f
diegowit <20098757@mail.wit.ie>
2024-03-16 06:10:32 +0000

    Main App file created

--- app/src/main/java/com/example/gokartandroidapplication/activities/GoKartListActivity.kt
+++ app/src/main/java/com/example/gokartandroidapplication/activities/GoKartListActivity.kt
@@ -1,15 +1,36 @@
 package com.example.gokartandroidapplication.activities
 
+
+import android.app.Activity
+import android.content.Intent
+import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuItem
+import android.view.ViewGroup
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+import com.example.gokartandroidapplication.databinding.ActivityGokartlistBinding
+//import com.example.gokartandroidapplication.databinding.CardGoKartBinding
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.gokartandroidapplication.R
 import com.example.gokartandroidapplication.main.MainApp
+
 class GoKartListActivity : AppCompatActivity() {
 
     lateinit var app: MainApp
+    private lateinit var binding: ActivityGokartlistBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityGokartlistBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         setContentView(R.layout.activity_gokartlist)
         app = application as MainApp
+
+
+
+
     }
 }
