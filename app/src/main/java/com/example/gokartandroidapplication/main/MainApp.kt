c075af4188c19c43497124437991cde62c96f62c
diegowit <20098757@mail.wit.ie>
2024-03-16 05:07:04 +0000

    Main App file created

--- app/src/main/java/com/example/gokartandroidapplication/main/MainApp.kt
+++ app/src/main/java/com/example/gokartandroidapplication/main/MainApp.kt
@@ -1,4 +1,14 @@
 package com.example.gokartandroidapplication.main
 
-class MainApp {
+import android.app.Application
+import timber.log.Timber
+import timber.log.Timber.i
+
+class MainApp: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        Timber.plant(Timber.DebugTree())
+        i("Go Kart App Started")
+    }
 }
