dd98ca2635651c7b67c94ca0aa42f0f69ca5fa34
diegowit <20098757@mail.wit.ie>
2024-03-17 03:18:22 +0000

    comments

--- app/src/main/java/com/example/gokartandroidapplication/adapters/GoKartAdapter.kt
+++ app/src/main/java/com/example/gokartandroidapplication/adapters/GoKartAdapter.kt
@@ -9,37 +9,51 @@ import com.example.gokartandroidapplication.models.GoKartModel
 import com.squareup.picasso.Picasso
 
 
+// Interface for handling click events on GoKart items
 interface GoKartListener {
+    // Function to be implemented to handle click events
     fun onGoKartClick(gokart: GoKartModel)
 }
 
-class GoKartAdapter constructor(private var gokarts: List<GoKartModel>,
-                                private val listener: GoKartListener)
-                                :
-    RecyclerView.Adapter<GoKartAdapter.MainHolder>() {
+// Adapter class for the RecyclerView to display GoKart items
+class GoKartAdapter(
+    // List of GoKartModel items to display
+    private var gokarts: List<GoKartModel>,
+    // Listener for click events on GoKart items
+    private val listener: GoKartListener
+) : RecyclerView.Adapter<GoKartAdapter.MainHolder>() {
 
+    // Create ViewHolder instances
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainHolder {
-        val binding = CardGokartBinding
-            .inflate(LayoutInflater.from(parent.context), parent, false)
-
+        // Inflate the layout for each item
+        val binding = CardGokartBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return MainHolder(binding)
     }
 
+    // Bind data to ViewHolder
     override fun onBindViewHolder(holder: MainHolder, position: Int) {
-        val gokart = gokarts[holder.adapterPosition]
+        val gokart = gokarts[position]
         holder.bind(gokart, listener)
     }
 
+    // Get the number of items in the list
     override fun getItemCount(): Int = gokarts.size
 
-    class MainHolder(private val binding : CardGokartBinding) :
+    // ViewHolder class to hold references to views
+    class MainHolder(private val binding: CardGokartBinding) :
         RecyclerView.ViewHolder(binding.root) {
 
+        // Bind data to views
         fun bind(gokart: GoKartModel, listener: GoKartListener) {
+            // Set data to TextViews
             binding.DriverName.text = gokart.name
             binding.CarModel.text = gokart.carModel
-            Picasso.get().load(gokart.image).resize(200,200).into(binding.imageIcon)
-            binding.root.setOnClickListener { listener.onGoKartClick(gokart)}
+
+            // Load image using Picasso and resize it
+            Picasso.get().load(gokart.image).resize(200, 200).into(binding.imageIcon)
+
+            // Set click listener to the root view
+            binding.root.setOnClickListener { listener.onGoKartClick(gokart) }
         }
     }
 }
